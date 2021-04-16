d371422686e26298ab832affafad68832657fb9f
ariesbaarde <acbaarde@sterlingpaper.com>
2021-04-16 16:42:47 +0800

    latest update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:88/myapp_api';
+$config['base_url'] = 'http://localhost/myapp_api';
 
 /*
 |--------------------------------------------------------------------------
